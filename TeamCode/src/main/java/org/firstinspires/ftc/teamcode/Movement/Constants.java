4c8434450a5661f1bc97a0bd69974d59d226772a
Roper <griffinroboticsaz@gmail.com>
2018-02-02 17:35:54 -0700

    Updated values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Movement/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Movement/Constants.java
@@ -7,6 +7,6 @@ package org.firstinspires.ftc.teamcode.Movement;
 public class Constants {
     public static final double OPEN_ARM_POSITION = 0.70;
     public static final double CLOSED_ARM_POSITION = 0.755;
-    public static final double COLOR_SERVO_LOWERED = 0.75;
-    public static final double COLOR_SERVO_RAISED = 0.06;
+    public static final double COLOR_SERVO_LOWERED = 0.25;
+    public static final double COLOR_SERVO_RAISED = 1;
 }
